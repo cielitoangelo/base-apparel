4be8d6649c1c9a2f5c2cb08273d23a27a9c9bfb3
Cielito Angelo Lozano <lozanoangelo01@gmail.com>
2021-08-12 20:10:06 +0800

    Mobile View

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("subscribe");
+const email = document.getElementById("email");
+const error = document.getElementById("error");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let validEmail = email.value.indexOf('@');
+    
+    if (email.value == "") {
+        error.removeAttribute("hidden");
+        email.style.cssText = "border-color: var(--rose);";
+    }
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+        location.href = '/index.html?email=' + email.value;
+    } else {
+        error.removeAttribute("hidden");
+        email.style.cssText = "border-color: var(--rose);";
+    }
+})
